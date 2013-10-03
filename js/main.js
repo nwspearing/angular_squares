a07d7a63253db8d5b78639deb051ac210e685388
Will Spearing <nwspearing@gmail.com>
2013-10-03 13:40:48 -0700

    Added X's and O's clicking using ng-click while still using event.target.innerHTML

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 // angular.module('newTicApp')
 //   .controller('MainCtrl', function ($scope) {
@@ -11,19 +11,42 @@
 
 
 
-<script>
+function TicTacCtrl($scope) {
+
+   var player_turn = 1;
+
+$scope.ticTacToe= [['','',''],['','',''],['','','']];
+
+$scope.clickSquare = function(row, column) { 
+   
+    console.log(row);
+    console.log(column);
+
+     
+
+     // $scope.cell.innerHTML = "X";
+     //      this.turnNum++;
+     // if(this.turnNum % 2 = 1)
+
+     // this.ticTacToe[row][column]= "X";
+     // else
+     //  this.ticTacToe[row][column]="O";
+      // event.target.innerHTML = "<img src='batman.jpg'>"
 
 
-    var player_turn = 1;
-
-
-var cellArray = [0,1,2,3,4,5,6,7,8];
 
 
 
+ 
 
 
-    function playBox() {
+// var cellArray = [0,1,2,3,4,5,6,7,8];
+
+
+
+
+
+    // function playBox() {
 
 
       if (event.target.innerHTML != "X" && event.target.innerHTML != "O") {
@@ -31,11 +54,13 @@ var cellArray = [0,1,2,3,4,5,6,7,8];
       if (player_turn % 2) {
 
       event.target.innerHTML="X";
+      this.ticTacToe[row][column]= "X";
       }
 
 
     else {
     event.target.innerHTML = "O";
+    this.ticTacToe[row][column]="O";
     }
     
     document.getElementById("notification").innerHTML = "Player "+((player_turn % 2)+1)+" - your turn!";
@@ -45,13 +70,13 @@ var cellArray = [0,1,2,3,4,5,6,7,8];
 
 
 
-      for (i=0; i<9; ++i)
-      {
+    //   for (i=0; i<9; ++i)
+    //   {
    
-      cellArray[i] =  document.getElementById("cell1_"+(i+1)).innerHTML;
+    //   cellArray[i] =  document.getElementById("cell1_"+(i+1)).innerHTML;
 
     
-    }   
+    // }   
 
   
 
@@ -135,8 +160,13 @@ var cellArray = [0,1,2,3,4,5,6,7,8];
           }
 
        // end playBox
-  }
+  // }
 
+  // end TicTacCtrl
+
+   };
+
+}
 
 
 
@@ -147,6 +177,169 @@ var cellArray = [0,1,2,3,4,5,6,7,8];
   }
 
 
+
+
+
+
+
+
+ // var player_turn = 1;
+ //  var fillbox = "";
+
+ //  function playerChoice($scope){
+    
+ //     if (player_turn % 2) {
+
+ //      fillbox="X";
+ //      }
+
+
+ //    else {
+ //    fillbox = "O";
+ //    }
+
+ //    player_turn++;
+
+ //    return fillbox;
+ //    }
+
+
+
+
+
+
+
+//     var player_turn = 1;
+
+
+// var cellArray = [0,1,2,3,4,5,6,7,8];
+
+
+
+
+
+//     function playBox() {
+
+
+//       if (event.target.innerHTML != "X" && event.target.innerHTML != "O") {
+
+//       if (player_turn % 2) {
+
+//       event.target.innerHTML="X";
+//       }
+
+
+//     else {
+//     event.target.innerHTML = "O";
+//     }
+    
+//     document.getElementById("notification").innerHTML = "Player "+((player_turn % 2)+1)+" - your turn!";
+
+//     player_turn++;
+
+
+
+
+//       for (i=0; i<9; ++i)
+//       {
+   
+//       cellArray[i] =  document.getElementById("cell1_"+(i+1)).innerHTML;
+
+    
+//     }   
+
   
 
-</script>
+   
+//     // WHAT HAPPENS IF YOU WIN / TIE
+
+         
+
+//       function win()
+//        {
+
+//         document.getElementById("message_overlay").style.zIndex = "2";
+//         document.getElementById("message_overlay").innerHTML = "Player "+ ((player_turn % 2)+1) + " wins! " +"<br/>"+ "<input type='button' id='btn2' value='Start Again' onclick='startAgain()'/>";
+//         document.getElementById("notification").innerHTML = "";
+     
+//         }
+
+//         function tie()
+//         {
+//            document.getElementById("message_overlay").style.zIndex = "2";
+//            document.getElementById("message_overlay").innerHTML = "You both tied! " +"<br/>"+ "<input type='button' id='btn2' value='Start Again' onclick='startAgain()'/>";
+//            document.getElementById("notification").innerHTML = "";
+
+//         }
+
+//       // end test to see if they clicked an already filled block
+//      } 
+
+
+//         // TEST FOR WIN
+
+//         if ( player_turn > 4 )
+//         {
+
+
+        
+         
+//           if (cellArray[0] == cellArray[3] && cellArray[3] != "" && cellArray[3] == cellArray[6])
+//               win();
+            
+//           else if (cellArray[1] == cellArray[4] && cellArray[4] != "" && cellArray[4] == cellArray[7])
+//               win();
+
+//           else if (cellArray[2] == cellArray[5] && cellArray[5] != "" && cellArray[5] == cellArray[8])
+//             { 
+             
+//               win();
+//             }
+//           else if (cellArray[0] == cellArray[1] && cellArray[1] != "" && cellArray[1] == cellArray[2])
+//             { 
+            
+//               win();
+//             }
+//           else if (cellArray[3] == cellArray[4] && cellArray[4] != "" && cellArray[4] == cellArray[5])
+//             { 
+            
+//               win();
+//             }
+//           else if (cellArray[6] == cellArray[7] && cellArray[7] != "" && cellArray[7] == cellArray[8])
+//             { 
+             
+//               win();
+//             }
+//           else if (cellArray[0] == cellArray[4] && cellArray[4] != "" && cellArray[4] == cellArray[8])
+//             { 
+            
+//               win();
+//             }
+//           else if (cellArray[2] == cellArray[4] && cellArray[4] != "" && cellArray[4] == cellArray[6])
+//             { 
+           
+//               win();
+//             } 
+//           else if (player_turn > 9)
+//             {
+             
+//               tie(); 
+
+//             }
+
+//           }
+
+//        // end playBox
+//   }
+
+
+
+
+//     function startAgain() {
+
+//     location.href="index.html";
+
+//   }
+
+
+  
